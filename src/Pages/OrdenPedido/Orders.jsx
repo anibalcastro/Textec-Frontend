20752a96995a2ff340717e053ed9a08fd8d803cb
anibalcastro <anibalcastro1515@gmail.com>
2023-09-23 15:53:57 -0600

    Ordenes

--- src/Pages/OrdenPedido/Orders.jsx
+++ src/Pages/OrdenPedido/Orders.jsx
@@ -1,71 +1,67 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
 import FilterOrders from "../../components/Filters/Filter-orders";
 
 const Orders = () => {
-    // State to store products
-    const [orders, setOrders] = useState([]);
+  // State to store products
+  const [orders, setOrders] = useState([]);
+  //Token activo
+  const token = Cookies.get("jwtToken");
+  const role = Cookies.get("role");
 
-    useEffect(() => {
-        const fetchOrders = () => {
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${token}`);
+  useEffect(() => {
+    const fetchOrders = () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
 
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
 
-            fetch("http://127.0.0.1:8000/api/v1/ordenes", requestOptions)
-                .then(response => response.json())
-                .then(result => {
-                    if (result.hasOwnProperty("data")) {
-                        const { data } = result;
-                        setOrders(data);
-                        localStorage.setItem('ordenes', JSON.stringify(data));
-                      } 
-                })
-                .catch(error => console.log('error', error));
-        }
+      fetch("https://api.textechsolutionscr.com/api/v1/ordenes", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.hasOwnProperty("ordenes")) {
+            const { ordenes } = result;
+            setOrders(ordenes);
+            localStorage.setItem("ordenes", JSON.stringify(ordenes));
+          }
+        })
+        .catch((error) => console.log("error", error));
+    };
 
+    fetchOrders();
+  }, []);
 
-
-        fetchOrders();
-    },[])
-
-    //Token activo
-    const token = Cookies.get("jwtToken");
-    const role = Cookies.get("role");
-
-    const validatePermissions = () => {
-        if (role === 'Admin' || role === 'Colaborador') {
-          return true;
-        }
-    
-        return false
+  const validatePermissions = () => {
+    if (role === "Admin" || role === "Colaborador") {
+      return true;
     }
-    
-    const contributorPermissions  = validatePermissions();
 
+    return false;
+  };
 
-    return(
-        <React.Fragment>
-           
-                <h2 className="titulo-encabezado">Orden de pedidos</h2>
-                <hr className="division"></hr>
+  const contributorPermissions = validatePermissions();
 
-                <div className="container mediciones-filtro">
-                    {contributorPermissions && (  <Link to='/orden/registro'>
-                        <button className="btn-registrar">Crear Orden</button>
-                    </Link>)}
-                </div>
-          
-            <FilterOrders datos={orders} />
+  return (
+    <React.Fragment>
+      <h2 className="titulo-encabezado">Orden de pedidos</h2>
+      <hr className="division"></hr>
 
-        </React.Fragment>
-    )
-}
+      <div className="container mediciones-filtro">
+        {contributorPermissions && (
+          <Link to="/orden/registro">
+            <button className="btn-registrar">Crear Orden</button>
+          </Link>
+        )}
+      </div>
+
+      <FilterOrders datos={orders} />
+    </React.Fragment>
+  );
+};
 
 export default Orders;
--- src/components/Filters/Filter-orders.jsx
+++ src/components/Filters/Filter-orders.jsx
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import Cookies from "js-cookie";
 
 const FilterOrders = ({ datos }) => {
   const [filter, setFilter] = useState("");
-  const [isCheckedCompany, setIsCheckedCompany] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 25; // Número de mediciones por página
+  const [company, setCompany] = useState([]);
+
+  const token = Cookies.get("jwtToken");
 
   useEffect(() => {
     setFilter(""); // Resetear el filtro cuando los datos cambian
     setCurrentPage(1); // Resetear la página cuando los datos cambian
+
+    const fetchCompany = () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://api.textechsolutionscr.com/api/v1/empresas",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.hasOwnProperty("data")) {
+            const { data } = result;
+            setCompany(data);
+          }
+        })
+        .catch((error) => console.log("error", error));
+    };
+
+    fetchCompany();
     loadingData();
-  
-
   }, []);
-
-  const handleSwitchCompanyChange = () => {
-    setIsCheckedCompany(!isCheckedCompany);
-  };
 
   const handleFiltroChange = (event) => {
     setFilter(event.target.value);
@@ -26,12 +49,9 @@ const FilterOrders = ({ datos }) => {
   };
 
   const filterData = () => {
-    if (isCheckedCompany) {
-      const datosFiltrados = datos.filter((dato) => {
-        const nombreProducto = `${dato.empresa}`;
-        return nombreProducto.toLowerCase().includes(filter.toLowerCase());
-      });
-      return datosFiltrados;
+    // Comprobar si datos está definido antes de filtrar
+    if (!datos) {
+      return [];
     }
 
     const datosFiltrados = datos.filter((dato) => {
@@ -67,11 +87,21 @@ const FilterOrders = ({ datos }) => {
     });
   };
 
+  const nameCompany = (companyId) => {
+    const empresaEncontrada = company.find(
+        (item) => parseInt(item.id) == parseInt(companyId)
+    );
+
+    if (empresaEncontrada) {
+        return empresaEncontrada.nombre_empresa;
+    } else {
+        return "Empresa no encontrada";
+    }
+};
+
+
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentFilteredItems = filterData()
-    .reverse()
-    .slice(indexOfFirstItem, indexOfLastItem);
 
   const totalPages = Math.ceil(filterData().length / itemsPerPage);
 
@@ -83,7 +113,7 @@ const FilterOrders = ({ datos }) => {
 
   return (
     <React.Fragment>
-     <div className="container-filtro-ordenes">
+      <div className="container-filtro-ordenes">
         <input
           className="filtro-pedidos"
           type="text"
@@ -92,64 +122,52 @@ const FilterOrders = ({ datos }) => {
           placeholder="Buscar Orden"
         />
 
-        <div className="d-flex align-items-center"> {/* Añade la clase 'align-items-center' para centrar verticalmente */}
+        <div className="d-flex align-items-center">
+          {" "}
+          {/* Añade la clase 'align-items-center' para centrar verticalmente */}
           <label>Buscar por: </label>
-
-          <div className="form-check form-switch">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="flexSwitchCheckDefault"
-              onChange={handleSwitchCompanyChange}
-              value={isCheckedCompany}
-            />
-            <label
-              className="form-check-label"
-              htmlFor="flexSwitchCheckDefault"
-            >
-              Empresa
-            </label>
-          </div>
         </div>
 
-      <table className="tabla-medidas">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Titulo</th>
-            <th>Empresa</th>
-            <th>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentFilteredItems.map((dato, index) => (
-            <tr key={index}>
-              <td>{iterador + index + 1}</td>
-              <td>
-                <Link
-                  className="link-nombre"
-                  to={`/orden/${dato.id}`}
-                >{`${dato.titulo}`}</Link>
-              </td>
-              <td>{dato.empresa}</td>
-              <td>₡{dato.fecha}</td>
+        <table className="tabla-medidas">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Titulo</th>
+              <th>Empresa</th>
+              <th>Estado</th>
+              <th>Fecha</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filterData().map((dato, index) => (
+              <tr key={index}>
+                <td>{iterador + index + 1}</td>
+                <td>
+                  <Link
+                    className="link-nombre"
+                    to={`/orden/${dato.id}`}
+                  >{`${dato.titulo}`}</Link>
+                </td>
+                <td>{nameCompany(dato.id_empresa)}</td>
+                <td>{dato.estado}</td>
+                <td>{dato.fecha_orden}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+          </div>
 
-      <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            className={currentPage === index + 1 ? "active" : ""}
-            onClick={() => handleClick(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
-      </div>
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              className={currentPage === index + 1 ? "active" : ""}
+              onClick={() => handleClick(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
     </React.Fragment>
   );
 };
