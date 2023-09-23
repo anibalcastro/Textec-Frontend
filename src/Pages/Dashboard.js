2cc60e9cc4d2a9eec7d6454c52a44622ff32cc9a
anibalcastro <anibalcastro1515@gmail.com>
2023-09-23 15:53:48 -0600

    Cantidad de ordenes por categoria

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -8,6 +8,11 @@ export default function Dashboard() {
   const [cantidadClientes, setCantidadCliente] = useState("");
   const [cantidadMedidas, setCantidadMedidas] = useState("");
   const [cantidadEmpresas, setCantidadEmpresas] = useState("");
+  const [cantidadOrdenes, setCantidadOrdenes] = useState("");
+  const [ordenesProceso, setOrdenesProceso] = useState("");
+  const [ordenesEntregadas, setOrdenesEntregadas] = useState("");
+  const [ordenesListas, setOrdenesListas] = useState("");
+  const [ordenesPendientes, setOrdenesPendientes] = useState("");
 
   useEffect(() => {
     const consultaCantidadClientes = async () => {
@@ -91,58 +96,77 @@ export default function Dashboard() {
         redirect: "follow",
       };
 
-      fetch("https://api.textechsolutionscr.com/api/v1/empresas/cantidad", requestOptions)
-        .then(response => response.json())
-        .then(result => {
+      fetch(
+        "https://api.textechsolutionscr.com/api/v1/empresas/cantidad",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
           let cantidad = result.cant_empresas;
-          setCantidadEmpresas(cantidad)})
-        .catch(error => console.log('error', error));
+          setCantidadEmpresas(cantidad);
+        })
+        .catch((error) => console.log("error", error));
+    };
+
+    const consultaCantidadPedidos = () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+    
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch("https://api.textechsolutionscr.com/api/v1/ordenes/cantidad", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+      
+          setOrdenesPendientes(result.cantidad_pendientes);
+          setOrdenesProceso(result.cantidad_enproceso);
+          setOrdenesListas(result.cantidad_listos);
+          setOrdenesEntregadas(result.cantidad_entragados);
+
+          const sumaTotal = parseInt(result.cantidad_pendientes) + parseInt(result.cantidad_enproceso) + parseInt(result.cantidad_listos) + parseInt(result.cantidad_entragados);
+
+          setCantidadOrdenes(sumaTotal);
+
+        })
+        .catch((error) => console.log("error", error));
     };
 
     consultaCantidadEmpresa();
     consultaCantidadClientes();
     consultaCantidadMedidas();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    consultaCantidadPedidos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <React.Fragment>
-  
-        <h2 className="titulo-encabezado">Inicio</h2>
+      <h2 className="titulo-encabezado">Inicio</h2>
 
-        <div className="information">
-          <Card titulo="Orden de Pedidos" numero="10" color="#6d949c" />
+      <div className="information">
+        <Card titulo="Orden de Pedidos" numero={cantidadOrdenes} color="#6d949c" />
 
-          <Card
-            titulo="Orden de Pedidos Pendientes"
-            numero="5"
-            color="#94744C"
-          />
+        <Card titulo="Orden de Pedidos Pendientes" numero={ordenesPendientes} color="#94744C" />
 
-          <Card
-            titulo="Orden de Pedidos En Proceso"
-            numero="5"
-            color="#6d949c"
-          />
+        <Card titulo="Orden de Pedidos En Proceso" numero={ordenesProceso} color="#6d949c" />
 
-          <Card
-            titulo="Orden de Pedidos Entregados"
-            numero="5"
-            color="#94744C"
-          />
+        <Card titulo="Orden de Pedidos Entregados" numero={ordenesEntregadas} color="#94744C" />
 
-          <Card titulo="Orden de Pedidos Listos" numero="5" color="#6d949c" />
+        <Card titulo="Orden de Pedidos Listos" numero={ordenesListas} color="#6d949c" />
 
-          <Card titulo="Medidas" numero={cantidadMedidas} color="#94744C" />
+        <Card titulo="Medidas" numero={cantidadMedidas} color="#94744C" />
 
-          <Card titulo="Clientes" numero={cantidadClientes} color="#6d949c" />
+        <Card titulo="Clientes" numero={cantidadClientes} color="#6d949c" />
 
-          <Card titulo="Empresas" numero={cantidadEmpresas} color="#94744C" />
-        </div>
-        <div className="container-logo">
-          <img src={Logo} className="logo-textec" alt="Logo" />
-        </div>
-    
+        <Card titulo="Empresas" numero={cantidadEmpresas} color="#94744C" />
+      </div>
+      <div className="container-logo">
+        <img src={Logo} className="logo-textec" alt="Logo" />
+      </div>
     </React.Fragment>
   );
 }
