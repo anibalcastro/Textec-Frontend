688ddbf1329e41086580d8109a0d4f9f27e4977c
anibalcastro <anibalcastro1515@gmail.com>
2023-10-27 21:02:50 -0600

    Detalle de la orden

--- src/Pages/OrdenPedido/OrderDetail.jsx
+++ src/Pages/OrdenPedido/OrderDetail.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Header from "../../components/Header/Header";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 import Swal from "sweetalert2";
+import Detail from "../../components/OrderDetail/Detail";
 
 const OrderDetail = () => {
   const [order, setOrder] = useState({
@@ -193,14 +193,6 @@ const OrderDetail = () => {
     console.log("Descargar orden");
   };
 
-  /**
-   * Format input at currency CRC
-   */
-  const formatCurrencyCRC = new Intl.NumberFormat("es-CR", {
-    style: "currency",
-    currency: "CRC",
-  });
-
   const getProducts = () => {
     var myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`);
@@ -245,35 +237,6 @@ const OrderDetail = () => {
         }
       })
       .catch((error) => console.log("error", error));
-  };
-
-  /**
-   * Returns name of the company by Id
-   * @param {Integer} companyId
-   * @returns {String}
-   */
-  const nameCompany = (companyId) => {
-    const empresaEncontrada = company.find(
-      (item) => parseInt(item.id) == parseInt(companyId)
-    );
-
-    if (empresaEncontrada) {
-      return empresaEncontrada.nombre_empresa;
-    } else {
-      return "Empresa no encontrada";
-    }
-  };
-
-  const nameProduct = (productId) => {
-    const productFind = product.find(
-      (item) => parseInt(item.id) == parseInt(productId)
-    );
-
-    if (productFind) {
-      return productFind.nombre_producto;
-    } else {
-      return "Producto no encontrado";
-    }
   };
 
   /**
@@ -333,148 +296,10 @@ const OrderDetail = () => {
 
   return (
     <React.Fragment>
-      <Header title="Detalle de la orden" />
 
-      <div className="container form-contenedor">
-        <form className="form-registro-clientes">
-          <div className="div-inp">
-            <label htmlFor="password">Titulo:</label>
-            <input
-              type="text"
-              name="titulo"
-              id="titulo"
-              autoComplete="current-password"
-              value={order.titulo}
-              disabled
-            />
-          </div>
-
-          <div className="div-inp">
-            <label htmlFor="password">Empresa:</label>
-            <input
-              type="text"
-              name="buscarEmpresa"
-              id="cedula"
-              autoComplete="current-password"
-              value={nameCompany(order.id_empresa)}
-              disabled
-            />
-          </div>
-
-          <div className="div-inp">
-                        <label htmlFor="password">Comentario:</label>
-                        <textarea
-                          
-                            id="txtArea"
-                            name="descripcion"
-                            rows="5"
-                            cols="60"
-                            value={order.comentario}
-                            disabled
-                        ></textarea>
-                    </div>
-
-          <div className="div-inp">
-            <label htmlFor="password">Estado:</label>
-            <input
-              type="text"
-              name="cajero"
-              id="titulo"
-              autoComplete="current-password"
-              disabled
-              value={order.estado}
-              required
-            />
-          </div>
-
-          <div className="div-inp">
-            <label htmlFor="password">Fecha:</label>
-            <input
-              type="text"
-              name="cajero"
-              id="titulo"
-              autoComplete="current-password"
-              disabled
-              value={order.fecha_orden}
-              required
-            />
-          </div>
-
-          <div className="div-inp">
-            <label htmlFor="password">Vendedor:</label>
-            {invoice && invoice.length > 0 ? (
-              <input
-                type="text"
-                name="cajero"
-                id="titulo"
-                autoComplete="current-password"
-                disabled
-                value={invoice[0].cajero}
-                required
-              />
-            ) : (
-              <span>No hay datos de vendedor disponibles.</span>
-            )}
-          </div>
-        </form>
-      </div>
-
-      <hr></hr>
-
-      <Header title="Detalle del pedido" />
-
-      <table className="tabla-medidas">
-        <thead>
-          <tr>
-            <th>Producto</th>
-            <th>Descripción</th>
-            <th>Cantidad</th>
-            <th>Precio unitario</th>
-            <th>Precio total</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {detail.map((item) => (
-            <tr key={item.id_producto}>
-              <td>{nameProduct(item.id_producto)}</td>
-              <td>{item.descripcion}</td>
-              <td>{item.cantidad}</td>
-              <td>{item.subtotal / item.cantidad}</td>
-              <td>{formatCurrencyCRC.format(item.subtotal)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <hr></hr>
-
-      <Header title="Facturación" />
-      <table className="tabla-medidas">
-        <thead>
-          <tr>
-            <th>Subtotal</th>
-            <th>IVA 13%</th>
-            <th>Total</th>
-            <th>Monto pendiente</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {Array.isArray(invoice) &&
-            invoice.map((item) => (
-              <tr key={item.subtotal}>
-                <td>{formatCurrencyCRC.format(item.subtotal)}</td>
-                <td>{formatCurrencyCRC.format(item.iva)}</td>
-                <td>{formatCurrencyCRC.format(item.monto)}</td>
-                <td>{formatCurrencyCRC.format(item.saldo_restante)}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-
-      <hr></hr>
-
+      <Detail order={order} detail={detail} invoice={invoice} company={company} 
+      product={product} />
+     
       <div className="container botones-contenedor">
         <Link to="/orden">
           <button className="btn">Regresar</button>
