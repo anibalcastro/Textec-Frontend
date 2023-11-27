8f0b98e4af91126fcc0c9f919b57a0cfb7d36272
anibalcastro <anibalcastro1515@gmail.com>
2023-11-26 23:20:32 -0600

    Reporte de cuentas pendientes

---
+++ src/Pages/Reportes/AccountsReceivable.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+
+const AccountsReceivable = () => {
+  
+
+  const downloadPDF = () => {};
+
+  return (
+    <React.Fragment>
+      <Header title="Saldos pendientes" />
+
+      <div className="container mediciones-filtro">
+        <button className="btn-registrar" onClick={() => downloadPDF()}>
+          Descargar
+        </button>
+      </div>
+
+    </React.Fragment>
+  );
+};
+
+export default AccountsReceivable;
