3d6a0b9b1184afd0ff0b3dbf40e97962c4579798
anibalcastro <anibalcastro1515@gmail.com>
2023-12-07 23:23:12 -0600

    Detalle de la orden

--- src/Pages/OrdenPedido/OrderDetail.jsx
+++ src/Pages/OrdenPedido/OrderDetail.jsx
@@ -31,7 +31,7 @@ const OrderDetail = () => {
     getCompany();
     getProducts();
     loadingData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchOrder = () => {
@@ -128,6 +128,103 @@ const OrderDetail = () => {
     });
   };
 
+  const returnEmailCompany = (idCompany) => {
+    const data = company.find((company) => company.id === idCompany);
+
+    if (data) {
+      return data.email;
+    } else {
+      return "anibalcastro1515@gmail.com"; // Puedes manejar el caso donde la compañía no existe
+    }
+  };
+
+  const returnPhoneCompany = (idCompany) => {
+    const data = company.find((company) => company.id === idCompany);
+
+    if (data) {
+      return data.telefono_encargado;
+    } else {
+      return "85424471"; // Puedes manejar el caso donde la compañía no existe
+    }
+  };
+
+  const notify = (state) => {
+    Swal.fire({
+      title: "¿Cómo desea notificar a la empresa?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "WhatsApp",
+      confirmButtonColor: "black",
+      cancelButtonText: "Correo electrónico",
+      showCloseButton: true,
+      showCloseButtonText: "No notificar",
+      showCloseButtonColor: "black",
+      reverseButtons: true,
+    }).then((result) => {
+      const mensaje = `Estimado cliente, espero que se encuentre bien. Le informamos que su pedido se encuentra en estado ${state}. Estamos a su disposición para cualquier consulta. ¡Gracias por su preferencia!`;
+
+      if (result.isConfirmed) {
+        // Acción si el usuario elige WhatsApp
+        const phoneNumber = returnPhoneCompany(order.id_empresa).replace(/[\s-]+/g, ""); // Número de teléfono de destino
+        const encodedMessage = encodeURIComponent(mensaje);
+        const url = `https://api.whatsapp.com/send?phone=+506${phoneNumber}&text=${encodedMessage}`;
+
+        window.open(url, "_blank");
+        return true;
+
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        const email = returnEmailCompany(order.id_empresa);
+        sendEmail(email, mensaje);
+        return true;
+      } else if (result.dismiss === Swal.DismissReason.close) {
+        return true;
+      } else {
+        return true;
+      }
+    });
+  };
+
+  const sendEmail = (email, body) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    var formdata = new FormData();
+    formdata.append("email", email);
+    formdata.append("body", body);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://api.textechsolutionscr.com/api/v1/email/notificacion",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        const { mensaje } = result;
+
+        if (mensaje === "Correo electrónico enviado con éxito") {
+          Swal.fire(
+            "¡Email enviado con éxito!",
+            `Se ha enviado un email a ${email}!`,
+            "success"
+          );
+        } else {
+          Swal.fire(
+            "¡Error!",
+            `Ocurrio un error al enviar el email, intente luego!`,
+            "error"
+          );
+        }
+      })
+      .catch((error) => console.log("Error durante la petición:", error));
+  };
+
   const changeStateOrder = () => {
     let actualStateOrder = order.estado;
     const states = ["Pendiente", "En Proceso", "Listo", "Entregado"];
@@ -165,11 +262,17 @@ const OrderDetail = () => {
         .then((result) => {
           const { status, error } = result;
           if (parseInt(status) === 200) {
-            Swal.fire(
-              "Estado modificado!",
-              `Se ha a modificado el estado de la orden a ${nextState}!`,
-              "success"
-            );
+            let result = notify(nextState);
+
+            if(result === true){
+              Swal.fire(
+                "Estado modificado!",
+                `Se ha a modificado el estado de la orden a ${nextState}!`,
+                "success"
+              );
+            }
+
+
           } else {
             let errorMessage = "";
             for (const message of error) {
@@ -191,38 +294,40 @@ const OrderDetail = () => {
   };
 
   const downloadOrder = () => {
-    //Arreglar la reparación...
     Swal.fire({
-      title: 'Generando el PDF',
-      text: 'Espere un momento...',
+      title: "Generando el PDF",
+      text: "Espere un momento...",
       allowOutsideClick: false,
       onBeforeOpen: () => {
         Swal.showLoading();
-      }
+      },
     });
 
-      var myHeaders = new Headers();
-      myHeaders.append("Authorization", `Bearer  ${token}`);
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer  ${token}`);
 
-      var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-      };
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
 
-      fetch(`https://api.textechsolutionscr.com/api/v1/pdf/orden_pedido/${ordenId}`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-          const download_url = decodeURIComponent(result.download_url);
-          const downloadLink = document.createElement("a");
-          downloadLink.href = download_url;
-          downloadLink.target = "_self"; // Abrir en una nueva pestaña
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-          document.body.removeChild(downloadLink);
-          Swal.close();
-        })
-        .catch(error => console.log('error', error));
+    fetch(
+      `https://api.textechsolutionscr.com/api/v1/pdf/orden_pedido/${ordenId}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const download_url = decodeURIComponent(result.download_url);
+        const downloadLink = document.createElement("a");
+        downloadLink.href = download_url;
+        downloadLink.target = "_self"; // Abrir en una nueva pestaña
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+        Swal.close();
+      })
+      .catch((error) => console.log("error", error));
   };
 
   const getProducts = () => {
@@ -328,10 +433,16 @@ const OrderDetail = () => {
 
   return (
     <React.Fragment>
+      <Detail
+        order={order}
+        detail={detail}
+        invoice={invoice}
+        company={company}
+        product={product}
+        title={"Detalle de la orden"}
+        subtitle={"Detalle de la orden"}
+      />
 
-      <Detail order={order} detail={detail} invoice={invoice} company={company} 
-      product={product} title={"Detalle de la orden"} subtitle={"Detalle de la orden"}/>
-     
       <div className="container botones-contenedor">
         <Link to="/orden">
           <button className="btn">Regresar</button>
